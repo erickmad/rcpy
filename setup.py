fda7ce3ad5176fdac7862e6737d46965605a14ca
Erick Madrigal <erick.madrigal8089@gmail.com>
2025-11-20 00:35:38 +0100

    renamed package code folder to src

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="rcpy",
     version="0.1",
-    packages=find_packages(),  # automatically finds all packages
+    packages=find_packages("src"),
+    package_dir={"": "src"},
 )
